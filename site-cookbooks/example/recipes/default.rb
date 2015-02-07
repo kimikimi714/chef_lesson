ab86486d470cac1c4019516f14e9960ac5ef67d7
kimikimi714 <kimikimi714@gmail.com>
2015-02-08 02:18:41 +0900

    remiのバージョンをあげる epelはyum installで入るようになったしやめる

--- site-cookbooks/example/recipes/default.rb
+++ site-cookbooks/example/recipes/default.rb
@@ -29,18 +29,10 @@ package "yum" do
   action :upgrade
 end
 
-# add epel, remi repositories
-yum_repository 'epel' do
-  description 'Extra Packages for Enterprise Linux'
-  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
-  fastestmirror_enabled true
-  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
-  action :create
-end
-
+# add remi repositories
 yum_repository 'remi' do
   description 'Les RPM de Remi - Repository'
-  baseurl 'http://rpms.famillecollet.com/enterprise/6/remi/x86_64/'
+  baseurl 'http://rpms.famillecollet.com/enterprise/7/remi/x86_64/'
   gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
   fastestmirror_enabled true
   action :create
